5786e4fdc08f56a25fb3daa0e576fefae8cc9848
CateVillegas <cvillegas@mail.utdt.edu>
2024-06-22 20:58:04 -0300

    entrelazamiento y secciones

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,3 @@
-
 <script>
   import Scroller from "@sveltejs/svelte-scroller";
   import { onMount } from "svelte";
@@ -10,310 +9,147 @@
   import Stars from "./components/stars.svelte";
 
   // timeline
-  import { Timeline, TimelineElement } from 'svelte-animated-timeline';
+  import { Timeline, TimelineElement } from "svelte-animated-timeline";
 
-	// import WorkIcon from '$lib/components/WorkIcon.svelte';
-  
-
-  /* Variables para la data del medallero */
-  let deportistas = [];
-  let filteredDeportistas = [];
-
-  /* Variables para el scroller1 */
-  let count;
-  let index;
-  let offset;
-  let progress;
-  let top = 0.1;
-  let threshold = 0.5;
-  let bottom = 0.9;
-
-  /* Variables para el scroller 2 */
-  let count2;
-  let index2;
-  let offset2;
-  let progress2;
-  let top2 = 0.1;
-  let threshold2 = 0.5;
-  let bottom2 = 0.9;
-
-  /* Charts */
   let charts = {
-    0: "lines_01.png",
-    1: "lines_02.png",
-    2: "lines_03.png",
+    0: "entrelazamiento1.png",
+    1: "entrelazamiento2.png",
+    2: "entrelazamiento3.png",
+    3: "entrelazamiento4.png",
   };
 
-  let canvas, ctx;
+  let currentIndex = 0;
+  let currentImage = charts[currentIndex];
 
-  function drawBlochSphere(rotation = 0) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.save();
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(rotation);
-
-    ctx.beginPath();
-    ctx.arc(0, 0, 45, 0, Math.PI * 2);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.ellipse(0, 0, 45, 20, 0, 0, Math.PI);
-    ctx.stroke();
-
-    ctx.save();
-    ctx.setLineDash([5, 5]);
-    ctx.beginPath();
-    ctx.ellipse(0, 0, 45, 20, 0, Math.PI, 2 * Math.PI);
-    ctx.stroke();
-    ctx.restore();
-
-    ctx.beginPath();
-    ctx.ellipse(0, 0, 20, 45, 0, Math.PI / 2, 3 * Math.PI / 2);
-    ctx.stroke();
-
-    ctx.save();
-    ctx.setLineDash([5, 5]);
-    ctx.beginPath();
-    ctx.ellipse(0, 0, 20, 45, 0, 3 * Math.PI / 2, Math.PI / 2);
-    ctx.stroke();
-    ctx.restore();
-
-    ctx.beginPath();
-    ctx.arc(0, 0, 3, 0, Math.PI * 2);
-    ctx.fill();
-
-    ctx.lineWidth = 3;
-
-    ctx.strokeStyle = 'rgba(0, 0, 255, 0.3)';
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, -45);
-    ctx.stroke();
-
-    ctx.strokeStyle = 'rgba(0, 0, 255, 0.9)';
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, -15);
-    ctx.stroke();
-
-    ctx.restore();
-
-    ctx.lineWidth = 1.5;
-
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(30, -2);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(30, -2, 2, 0, Math.PI * 2);
-    ctx.fill();
+   // Function to change the image after 1 second delay
+  function changeImage() {
+    setTimeout(() => {
+      currentIndex = (currentIndex + 1) < 4 ? currentIndex + 1 : 0;
+      currentImage = charts[currentIndex];
+      changeImage(); // Call the function again to continue the loop
+    }, 60);
   }
 
+  // Call the function to change the image when the component is mounted
   onMount(() => {
-    d3.csv("./data/deportistas.csv", d3.autoType).then(data => {
-      deportistas = data;
-      filteredDeportistas = deportistas;
-    });
-
-    canvas = document.getElementById('bloch');
-    ctx = canvas.getContext('2d');
-
-    canvas.width = 100;
-    canvas.height = 100;
-
-    drawBlochSphere();
-
-    window.addEventListener('scroll', handleScroll);
+    changeImage();
   });
-
-  function handleScroll() {
-    const rotation = window.scrollY * 0.01; // Adjust the factor as needed
-    drawBlochSphere(rotation);
-  }
-
-  $: {
-    // Es un observer que se ejecuta cuando cambia el valor de index
-    switch (index) {
-      case 0:
-        filteredDeportistas = deportistas;
-        break;
-      case 1:
-        filteredDeportistas = deportistas.filter(d => d.genero === "F");
-        break;
-      case 2:
-        filteredDeportistas = deportistas.filter(d => d.genero === "M");
-        break;
-      case 3:
-        filteredDeportistas = deportistas.filter(
-          d => d.continente === "América",
-        );
-        break;
-      default:
-        filteredDeportistas = deportistas;
-    }
-    console.log(filteredDeportistas);
-  }
 </script>
 
 <main>
-  <Stars/>
-
-  <section class="header">
-
-    <h1 class="headline"> Computación Cuántica</h1>
-  </section>
+  
+  <div class="header section">
+    <h1 class="headline">Computación Cuántica</h1>
+    <Stars />
+  </div>
   <!-- <canvas id="bloch"></canvas> -->
 
-  <section class=intro-section>
-    <div class="contenido">
-      <p>
-        La computación cuántica es un campo de estudio que aplica los principios de la mecánica cuántica, 
-        una rama de la física que describe el comportamiento de las partículas a nivel subatómico, 
-        a la informática.
-        A diferencia de la computación clásica, que utiliza bits que pueden estar en uno de dos estados (0 o 1), 
-        la computación cuántica utiliza bits cuánticos, o qubits, que pueden existir en múltiples 
-        estados a la vez.
-      <p>
-      <img src="public/images/quantumComputer.png" alt="">
-    </div>
-    <div class="contenido">
-      <p>
-        Los Qubits permiten a los ordenadores cuánticos procesar una gran cantidad de información simultáneamente. 
-        Esto tiene el potencial de resolver problemas que son inabordables para los ordenadores clásicos, 
-        abriendo nuevas posibilidades en campos como la criptografía, la optimización de sistemas complejos, la simulación 
-        de materiales y medicamentos a nivel molecular, entre otros. 
-        
-        Sin embargo, la computación cuántica todavía está en sus primeras etapas de desarrollo y presenta muchos desafíos 
-        técnicos y teóricos.
-      </p>
-      <img src="public/images/quantumComputer.png" alt="">
-    </div>
+  <div class="contenido section">
+    <p>
+      La computación cuántica es un campo de estudio que aplica los principios
+      de la mecánica cuántica, una rama de la física que describe el
+      comportamiento de las partículas a nivel subatómico, a la informática. A
+      diferencia de la computación clásica, que utiliza bits que pueden estar en
+      uno de dos estados (0 o 1), la computación cuántica utiliza bits
+      cuánticos, o qubits, que pueden existir en múltiples estados a la vez.
+    </p>
+    <p>
+      <img src="public/images/quantumComputer.png" alt="" />
+    </p>
+  </div>
 
-  </section>
+  <div class="contenido section">
+    <p>
+      Los Qubits permiten a los ordenadores cuánticos procesar una gran cantidad
+      de información simultáneamente. Esto tiene el potencial de resolver
+      problemas que son inabordables para los ordenadores clásicos, abriendo
+      nuevas posibilidades en campos como la criptografía, la optimización de
+      sistemas complejos, la simulación de materiales y medicamentos a nivel
+      molecular, entre otros. Sin embargo, la computación cuántica todavía está
+      en sus primeras etapas de desarrollo y presenta muchos desafíos técnicos y
+      teóricos.
+    </p>
+  </div>
+
   <!-- Nueva sección sobre entrelazamiento y superposicion -->
-  <section class="Superpos-section">
+  <section class="Superpos-section section">
     <div class="contenido">
-      <h2>Superposición</h2>
-        <p>
-          las partículas cuánticas son una combinación de todos los estados posibles. Fluctúan hasta que se observan y se miden. Una forma de ilustrar la diferencia entre la posición binaria y la superposición es imaginar una moneda. Los bits clásicos se miden "volteando la moneda" y obteniendo cara o cruz. Sin embargo, si fuese posible mirar una moneda y ver las dos caras a la vez, y todos los estados entre medias, la moneda estaría en una superposición.     
-        </p>
-    </div>
-  </section>
-  <section class="entrelazamiento-section">
-    <div class="contenido">
-      <h2>Entrelazamiento</h2>
+      <h2 class = "subtitulo">Superposición</h2>
       <p>
-        El entrelazamiento cuántico es un fenómeno en el que dos qubits (o dos o más partículas cuánticas cualesquiera) se entrelazan de tal forma que el estado de una partícula no puede describirse independientemente del estado de la otra, sin importar la distancia que las separe.      
+        las partículas cuánticas son una combinación de todos los estados
+        posibles. Fluctúan hasta que se observan y se miden. Una forma de
+        ilustrar la diferencia entre la posición binaria y la superposición es
+        imaginar una moneda. Los bits clásicos se miden "volteando la moneda" y
+        obteniendo cara o cruz. Sin embargo, si fuese posible mirar una moneda y
+        ver las dos caras a la vez, y todos los estados entre medias, la moneda
+        estaría en una superposición.
       </p>
     </div>
   </section>
-
-
-  {#if progress < 1}
-  <DebugScroller
-    index={index}
-    count={count}
-    offset={offset}
-    progress={progress}
-  />
-  {/if}
-  <!-- Primer scroller
-  <Scroller
-    top={top}
-    threshold={threshold}
-    bottom={bottom}
-    bind:count={count}
-    bind:index={index}
-    bind:offset={offset}
-    bind:progress={progress}
-  >
-    <div slot="background">
-      <Medallero deportistas={filteredDeportistas} />
+  <div class="section entrelazamiento-section">
+    <!-- <Medallero/> -->
+    <div class="contexto-entrelazamiento">
+      <h2 class = "subtitulo">Entrelazamiento</h2>
+      <p class = "texto">
+        El entrelazamiento cuántico es un fenómeno en el que dos qubits (o dos o
+        más partículas cuánticas cualesquiera) se entrelazan de tal forma que el
+        estado de una partícula no puede describirse independientemente del
+        estado de la otra, sin importar la distancia que las separe.
+      </p>
     </div>
-    <div slot="foreground" class="foreground_container">
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Todos los deportistas</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Deportistas femeninas</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Deportistas masculinos</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Deportistas americanos</p>
-        </div>
-      </section>
+    <div>
+      <img src="/images/{currentImage}" alt="chart {0}" class="charts" />
     </div>
-  </Scroller>
-
-  <div class="lorem_ipsum">
-    <Loremipsum />
-  </div> -->
-
-  <!-- Segundo scroller -->
-  <Scroller
-    top={top2}
-    threshold={threshold2}
-    bottom={bottom2}
-    bind:count={count2}
-    bind:index={index2}
-    bind:offset={offset2}
-    bind:progress={progress2}
-  >
-    <div slot="background" class="image_container">
-      <img src="/images/{charts[index2]}" alt="chart {index2}" class="charts"
-      />
-    </div>
-    <div slot="foreground" class="foreground_container">
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index2 + 1}</h3>
-          <p>Gráfico 1</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index2 + 1}</h3>
-          <p>Gráfico 1</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index2 + 1}</h3>
-          <p>Gráfico 1</p>
-        </div>
-      </section>
-    </div>
-  </Scroller>
+  </div>
 </main>
 
-<div class="lorem_ipsum">
+<div class="lorem_ipsum section">
   <Loremipsum />
 </div>
 
 <style>
-.header {
+  .texto {
+    font-size: 18px;
+    line-height: 1.6;
+    position: relative;
+    text-align: justify;
+  }
+  .subtitulo {
+    font-size: 36px;
+    margin-bottom:-10px;
+    position: relative;
+  }
+
+  .charts {
+    width: 50vw;
+  }
+
+  /* Estilos para la sección de Entrelazamiento */
+  .entrelazamiento-section {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .contexto-entrelazamiento {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 50vw;
+    margin-bottom: 20px;
+  }
+  .section {
+    scroll-snap-align: start;
+    height: 100vh;
+  }
+  .header {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    margin-top: 120px;
-    margin-bottom: 120px;
     z-index: 1;
+    width: 100%;
   }
 
   /*  Animación color del título*/
@@ -338,50 +174,16 @@
   .headline {
     font-size: 60px;
     line-height: 1.2;
-    font-weight:bolder;
+    font-weight: bolder;
     text-align: center;
-    margin: 20px;
-    background: linear-gradient(90deg,#7DEFE0, #87FF8C, #FF667A, #3D46F2);
+    background: linear-gradient(90deg, #7defe0, #87ff8c, #ff667a, #3d46f2);
     background-size: 200% 200%;
     background-clip: text;
     -webkit-background-clip: text;
     color: transparent;
     animation: gradientShift 4s cubic-bezier(0.42, 0, 0.58, 1) infinite;
     z-index: 1;
-  }
-
-
-
-  /* Estilos para el scroller */
-  .foreground_container {
-    pointer-events: none;
-    padding-left: 50%;
-  }
-
-  .step_foreground {
-    display: flex;
-    justify-content: end;
-    align-items: center;
-    height: 100vh;
-    border: 1px solid rgba(0, 0, 0, 0.4);
-    color: white;
-    padding: 1em;
-    margin: 0 0 2em 0;
-  }
-  .epi_foreground {
-    padding: 20px;
-    max-width: 150px;
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-  .lorem_ipsum {
-    margin: 100px auto;
-    max-width: 740px;
-  }
-  .image_container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+    margin: 0;
   }
 
   /* Introducción */
@@ -417,9 +219,11 @@
   }
   /* Estilos para la sección de Superposición */
   .Superpos-section {
-   
-    padding: 50px 20px;
+    /* background-color: #7DEFE0; */
     position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .Superpos-section .contenido {
@@ -432,12 +236,10 @@
   }
 
   .Superpos-section h2 {
-  
     font-size: 36px;
     margin-bottom: 10px;
     position: relative;
     transform: translate(93%, -240%);
-    
   }
 
   .Superpos-section p {
@@ -445,66 +247,6 @@
     line-height: 1.6;
     margin-bottom: 20px;
     position: relative;
-    transform: translate( -10%, 10%);
+    transform: translate(-10%, 10%);
   }
-
-  /* Estilos para la sección de Entrelazamiento */
-  .entrelazamiento-section {
-    position: relative;
-    padding: 50px 20px;
-    /* Ajusta el padding según sea necesario */
-  }
-
-  .entrelazamiento-section .contenido {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
-    gap: 20px;
-    position: relative;
-    margin-bottom: 20px;
-  }
-
-  .entrelazamiento-section h2 {
-    font-size: 36px;
-    position: relative;
-    margin-bottom: 10px;
-  }
-
-  .entrelazamiento-section p {
-    font-size: 18px;
-    position: relative;
-    line-height: 1.6;
-    margin-bottom: 20px;
-  }
-
-  /* Estilos para la sección de Entrelazamiento */
-  .entrelazamiento-section {
-  
-    padding: 50px 20px;
-    position: relative;
-  }
-
-  .entrelazamiento-section .contenido {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
-    gap: 20px;
-    margin-bottom: 20px;
-    position: relative; /* Puede ser eliminado si no se necesita */
-  }
-
-  .entrelazamiento-section h2 {
-    font-size: 36px;
-    margin-bottom: 10px;
-    transform: translate(3%, -190%);
-  }
-
-  .entrelazamiento-section p {
-    font-size: 18px;
-    
-    line-height: 1.6;
-    margin-bottom: 20px;
-    transform: translate( -100%, 10%);
-  }
-  
 </style>
--- src/components/stars.svelte
+++ src/components/stars.svelte
@@ -113,10 +113,8 @@
       top: 0;
       left: 0;
       width: 100%;
-      height: 100%;
+      height: 100;
       z-index: 0;
-    
-      display: block;
     }
   </style>
   
