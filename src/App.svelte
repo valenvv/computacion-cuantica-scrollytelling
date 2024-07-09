c90502377e5876d774765d71bbc59cdadc21ac91
valenvv <valentinavitetta5@gmail.com>
2024-07-09 19:19:19 -0300

    footer

--- src/App.svelte
+++ src/App.svelte
@@ -560,7 +560,42 @@
   </div>
   </div>
 
-  
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>Visualzación de Datos | Entrega Final</h4>
+          <ul>
+            <li><a href="#">Universidad Torcuato Di Tella</a></li>
+            <li><a href="#">Caterina Villegas</a></li>
+            <li><a href="#">Valentina Vitetta</a></li>
+
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Bibliografía</h4>
+          <ul>
+            <li><a target="_blank" href="https://www.ibm.com/es-es/topics/quantum-computing">Computación Cuántica - IBM</a></li>
+            <li><a target="_blank" href="https://azure.microsoft.com/es-es/resources/cloud-computing-dictionary/what-is-quantum-computing">Computación Cuántica - Microsoft</a></li>
+            <li><a target="_blank" href="https://blogthinkbig.com/que-son-qubits-y-tipos">¿Qué son los Qubits?</a></li>
+            <li><a target="_blank" href="https://www.jsr.org/hs/index.php/path/article/download/2236/975/14010">Comparación entre Qubits</a></li>
+            <li><a target="_blank" href="https://quantumpedia.uk/a-brief-history-of-quantum-computing-e0bbd05893d0#:~:text=The%20Theoretical%20Foundations%20of%20Quantum,Quantum%20Computers%20(2000%E2%80%932021)">Historia de la computación Cuántica</a></li>
+            <li><a target="_blank" href="https://www.researchgate.net/publication/336341290_Trends_in_Quantum_Computing">Tendencias en la Computación Cuántica</a></li>
+            <li><a target="_blank" href="https://www.ibm.com/es-es/topics/quantum-computing">Computación Cuántica en el Machine Learning</a></li>
+            <li><a target="_blank" href="https://www.ibm.com/es-es/topics/quantum-computing">Desafíos de la Computación Cuántica</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+ </footer>
 </main>
 
 <style>
@@ -1156,6 +1191,92 @@
     background-color: rgba(0, 0, 0, 0.877);
   }
 
+  /* Estilos Footer */
+  .container{
+    max-width: 1170px;
+    margin:auto;
+  }
+  .row{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  ul{
+    list-style: none;
+  }
+  .footer{
+    background-color: #2b2b2d8c;
+      padding: 70px 0;
+  }
+  .footer-col{
+    width: 30%;
+    padding: 0 15px;
+  }
+  .footer-col h4{
+    font-size: 18px;
+    color: #d7b6ff;
+    text-transform: capitalize;
+    margin-bottom: 35px;
+    font-weight: 500;
+    position: relative;
+    font-family: "Arp";
+  }
+  .footer-col h4::before{
+    content: '';
+    position: absolute;
+    left:0;
+    bottom: -10px;
+    background-color: #4a00a5;
+    height: 3px;
+    box-sizing: border-box;
+    width: 50px;
+  }
+  .footer-col ul li:not(:last-child){
+    margin-bottom: 10px;
+  }
+  .footer-col ul li a{
+    font-size: 16px;
+    text-transform: capitalize;
+    color: #ffffff;
+    text-decoration: none;
+    font-weight: 300;
+    color: #bbbbbb;
+    display: block;
+    transition: all 0.3s ease;
+  }
+  .footer-col ul li a:hover{
+    color: #ffffff;
+    padding-left: 8px;
+  }
+  .footer-col .social-links a{
+    display: inline-block;
+    height: 40px;
+    width: 40px;
+    background-color: rgba(255,255,255,0.2);
+    margin:0 10px 10px 0;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 50%;
+    color: #ffffff;
+    transition: all 0.5s ease;
+  }
+  .footer-col .social-links a:hover{
+    color: #24262b;
+    background-color: #ffffff;
+  }
+
+  /*responsive*/
+  @media(max-width: 767px){
+    .footer-col{
+      width: 50%;
+      margin-bottom: 30px;
+  }
+  }
+  @media(max-width: 574px){
+    .footer-col{
+      width: 100%;
+  }
+  }
   
 </style>
   
